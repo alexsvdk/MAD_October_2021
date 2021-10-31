8f61e0f49674f0c6fcc5d390ee2075de8b6f6256
Aleksey Shvedchikov <a1exs@yandex-team.ru>
2021-10-31 18:39:59 +0300

    remove old archive

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,8 +6,7 @@ class Constants {
   Constants._();
   static const baseUrl = 'http://45.144.179.101/scare-me/api/mobile/';
 
-  static const defaultToken =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYjUyOGZlZi1iYjY3LTQzNjMtYWU4Ni0wM2JjZTA5YWY3YTAiLCJpc3MiOiJodHRwOi8vMC4wLjAuMDo4MDgwIiwiZXhwIjoxNjM1Njk0Njk5LCJ1c2VyIjoiZmI1MjhmZWYtYmI2Ny00MzYzLWFlODYtMDNiY2UwOWFmN2EwIn0.b430zGtM8wHUSFCkMQ185yxLkHD3pkK6GhHKs0VCHFg';
+  static const defaultToken = '';
 }
 
 //FONTS
